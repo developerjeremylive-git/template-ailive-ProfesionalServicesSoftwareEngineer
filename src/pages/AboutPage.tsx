c148c861f2d81adde3f695e0ac5adfc062cd5cdb
Jeremy Live <codelive007@gmail.com>
2025-04-02 12:38:01 -0600

    update info finish about

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -134,7 +134,7 @@ export default function AboutPage() {
 						className="mb-24"
 					>
 						<h2 className="text-3xl font-bold text-white text-center mb-12">
-							Experiencia en el Desarrollo de Software y Inteligencia Artificial
+							Áreas de Experticia
 						</h2>
 						<div className="grid grid-cols-1 md:grid-cols-3 gap-8">
 							{expertise.map((exp, index) => (
